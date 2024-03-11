a2628a3f31a9118d1b1b1d7004da9585b662d487
emmanuel <jonueldev@gmail.com>
2024-03-11 17:17:40 +0100

    fixed the webhook route error

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,16 +1,16 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-export function Post(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(request: Request) {
     const payingAcct = localStorage.getItem('payingAcct');
     if (!payingAcct) {
-        return res.status(400).json({ message: 'payingAcct not found in localStorage' });
+        return NextResponse.json({message: 'payingAcct not found in localStorage'} );
     }
 
-    const webhookPayingAcct = req.body.payingAcct;
+    const webhookPayingAcct = await request.json()
 
-    if (payingAcct === webhookPayingAcct) {
-        res.status(200).json({ message: 'Webhook processed successfully' });
+    if (payingAcct === webhookPayingAcct?.payingAcct) {
+        return NextResponse.json({ message: 'Webhook processed successfully' });
     } else {
-        res.status(403).json({ message: 'Unauthorized' });
+        return NextResponse.json({ message: 'Unauthorized'});
     }
 }
