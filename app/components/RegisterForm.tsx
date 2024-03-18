efa743437d692f95f4f46299cf5f66bc0050f22e
Emmanuel uzoezie <uzoezieemmanuel@gmail.com>
2024-03-18 13:27:10 +0100

    edit the reciever phone

--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { useState } from 'react'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
 import React, { useEffect } from 'react'
@@ -7,6 +8,7 @@ import { toast } from "react-hot-toast"
 
 const RegisterForm = () => { 
     const router = useRouter()
+    const [errorrMessage, setErrorMessage] = useState(true)
 
     const {
         register,
@@ -35,6 +37,7 @@ const RegisterForm = () => {
         } catch (error) {
             console.error('Error:', error);
             toast.error('Error', { id: notification });
+            setErrorMessage(true)
         }
     };
 
@@ -47,13 +50,14 @@ const RegisterForm = () => {
   return (
       <div className='h-full flex-1 py-2 flex items-center justify-center'>
         <div className='h-full'>
-            <h2 className='text-center py-4'>Please fill the form</h2>
-              <form onSubmit={handleSubmit(onSubmit)} className="min-w-[400px]">
-                  <input {...register("merchantPublicKey", { required: true })} type="text" className='border-2 w-full rounded-lg p-2 outline-none border-gray-400'/>
-                  {errors.merchantPublicKey && <span>This field is required</span>}
+            <h2 className='text-center '>Enter reciever public address</h2>
+              <form onSubmit={handleSubmit(onSubmit)} className="min-w-[400px] px-4">
+                  <input {...register("merchantPublicKey", { required: true })} type="text" className={` w-full border-2 rounded-lg p-2 outline-none ${errorrMessage ? " border-red-400" : " border-gray-400"}`}/>
+                  {errors.merchantPublicKey && <span className='text-red-500'>This field is required</span>}
+                  {errorrMessage && <p className='text-[11px] text-red-600'>Wallet address is not valid</p>}
 
-                  <div className='flex justify-center pt-2'>
-                      <input type="submit" className='p-2 bg-blue-400 px-10 rounded-md font-semibold font-serif' />
+                  <div className='flex justify-center pt-6'>
+                      <input type="submit" className={`p-2 bg-blue-400 px-10 rounded-md font-semibold font-serif`} />
                   </div>
               </form>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "./components/RegisterForm";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h2>Stable pay</h2>\
+      <h2>Stable pay</h2>
       <RegisterForm />
     </main>
   );
