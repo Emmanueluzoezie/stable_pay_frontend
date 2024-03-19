1ea4883c425731d0eadd4088c902b774ebb1a1f1
Emmanuel uzoezie <87155142+Emmanueluzoezie@users.noreply.github.com>
2024-03-19 15:43:11 +0000

    fix router issue

--- app/components/QrCodeForm.tsx
+++ app/components/QrCodeForm.tsx
@@ -1,127 +1,158 @@
-"use client"
-import { createQR } from '@/utils/createQr'
-import QRCodeStyling from '@solana/qr-code-styling'
-import { LAMPORTS_PER_SOL } from '@solana/web3.js'
-import axios from 'axios'
-import React, { useRef, useState, useEffect } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { toast } from "react-hot-toast"
-import io from "socket.io-client"
+"use client";
+import { createQR } from "@/utils/createQr";
+import QRCodeStyling from "@solana/qr-code-styling";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
+import axios from "axios";
+import React, { useRef, useState, useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import io from "socket.io-client";
 
 const QrCodeForm = () => {
-    const [qrCodeUrl, setQrCodeUrl] = useState<QRCodeStyling>()
-    const qrRef = useRef<HTMLDivElement>(null)
-    
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<QrCode>()
-    
-    const onSubmit = async (data: QrCode) => {
-        const notification = toast.loading('Creating qr code...');
-        try {
-            const payingAccount = localStorage.getItem('payAccount');
-            if (!payingAccount) {
-                return;
-            }
+  const [qrCodeUrl, setQrCodeUrl] = useState<QRCodeStyling>();
+  const [findingError, setFindingError] = useState<any>();
+  const qrRef = useRef<HTMLDivElement>(null);
 
-            const requestBody: QrCodes = {
-                recipient: payingAccount.toString(),
-                refrences: [`${data.refrences}`],
-                amount: Number(data.amount),
-                label: data.label,
-                splToken: data.splToken,
-                message: data.message,
-                memo: data.memo,
-            }
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<QrCode>();
 
-            const response = await axios.post("https://stable-pay-production.up.railway.app/tx/createQR", requestBody);
-            const qrCode = createQR(
-                response.data, 
-                300,
-                "transparent"
-            )
+  const onSubmit = async (data: QrCode) => {
+    const notification = toast.loading("Creating qr code...");
+    try {
+      const payingAccount = localStorage.getItem("payAccount");
+      if (!payingAccount) {
+        return;
+      }
 
-            if (qrRef.current) {
-                qrRef.current.innerHTML = "";
-                qrCode.append(qrRef.current);
-            }
+      const requestBody: QrCodes = {
+        recipient: payingAccount.toString(),
+        refrences: [`${data.refrences}`],
+        amount: Number(data.amount),
+        label: data.label,
+        splToken: data.splToken,
+        message: data.message,
+        memo: data.memo,
+      };
 
-            toast.success('Qrcode is Successfully created...', {id: notification});
-        } catch (error) {
-            toast.error('An error occur while creating QR code.', { id: notification });
-        }
+      const response = await axios.post(
+        "https://stable-pay-production.up.railway.app/tx/createQR",
+        requestBody,
+      );
+      const qrCode = createQR(response.data, 300, "transparent");
+
+      if (qrRef.current) {
+        qrRef.current.innerHTML = "";
+        qrCode.append(qrRef.current);
+      }
+
+      toast.success("Qrcode is Successfully created...", { id: notification });
+    } catch (error) {
+      setFindingError(error);
+      console.log(error);
+      toast.error("An error occur while creating QR code.", {
+        id: notification,
+      });
     }
+  };
 
-    useEffect(() => {
-        const socket = io("http://localhost:4000");
+  useEffect(() => {
+    const socket = io("http://localhost:4000");
 
-        socket.on("connect", () => {
-            console.log("Connected to WebSocket server");
-        });
+    socket.on("connect", () => {
+      console.log("Connected to WebSocket server");
+    });
 
-        socket.on("data", (data: any) => {
-            console.log("Received data:", data);
-            toast.success(`Received payment from ${JSON.stringify(data.sender)}`);
-        });
+    socket.on("data", (data: any) => {
+      console.log("Received data:", data);
+      toast.success(`Received payment from ${JSON.stringify(data.sender)}`);
+    });
 
-        return () => {
-            socket.disconnect();
-        };
-    }, []);
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
 
+  return (
+    <div className="h-full flex-1 pt-2 pb-10 flex items-center justify-center">
+      <div className="h-full">
+        {/* <p> djjdjdj{findingError}</p> */}
+        {qrRef && (
+          <div className="py-4 flex justify-center">
+            <div ref={qrRef} />
+          </div>
+        )}
+        <h2 className="text-center py-4">Please fill the form</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="min-w-[400px]">
+          <div className="py-2">
+            <label className="font-mono pl-4 text-sm">Reference</label>
+            <input
+              {...register("refrences", { required: true })}
+              type="text"
+              className="border-2 w-full rounded-lg p-2 outline-none border-gray-400"
+            />
+            {errors.refrences && <span> Reference field is required</span>}
+          </div>
+          <div className="py-2">
+            <label className="font-mono pl-4 text-sm">Amount</label>
+            <input
+              {...register("amount", { required: true })}
+              type="text"
+              className="border-2 w-full rounded-lg p-2 outline-none border-gray-400"
+            />
+            {errors.amount && <span>Amount field is required</span>}
+          </div>
+          <div className="py-2">
+            <label className="font-mono pl-4 text-sm">Label</label>
+            <input
+              {...register("label", { required: true })}
+              type="text"
+              className="border-2 w-full rounded-lg p-2 outline-none border-gray-400"
+            />
+            {errors.label && <span>Memo field is required</span>}
+          </div>
 
-    return (
-        <div className='h-full flex-1 pt-2 pb-10 flex items-center justify-center'>
-            <div className='h-full'>
-                {qrRef && 
-                    <div className='py-4 flex justify-center'>
-                        <div ref={qrRef} />
-                    </div>
-                }
-                <h2 className='text-center py-4'>Please fill the form</h2>
-                <form onSubmit={handleSubmit(onSubmit)} className="min-w-[400px]">
-                    <div className='py-2'>
-                        <label className='font-mono pl-4 text-sm'>Reference</label>
-                        <input {...register("refrences", { required: true })} type="text" className='border-2 w-full rounded-lg p-2 outline-none border-gray-400' />
-                        {errors.refrences && <span> Reference field is required</span>}
-                    </div>
-                    <div className='py-2'>
-                        <label className='font-mono pl-4 text-sm'>Amount</label>
-                        <input {...register("amount", { required: true })} type="text" className='border-2 w-full rounded-lg p-2 outline-none border-gray-400' />
-                        {errors.amount && <span>Amount field is required</span>}
-                    </div>
-                    <div className='py-2'>
-                        <label className='font-mono pl-4 text-sm'>Label</label>
-                        <input {...register("label", { required: true })} type="text" className='border-2 w-full rounded-lg p-2 outline-none border-gray-400' />
-                        {errors.label && <span>Memo field is required</span>}
-                    </div>
+          <div className="py-2">
+            <label className="font-mono pl-4 text-sm">Message</label>
+            <input
+              {...register("message", { required: true })}
+              type="text"
+              className="border-2 w-full rounded-lg p-2 outline-none border-gray-400"
+            />
+            {errors.memo && <span>Message field is required</span>}
+          </div>
+          <div className="py-2">
+            <label className="font-mono pl-4 text-sm">Token</label>
+            <input
+              {...register("splToken", { required: true })}
+              type="text"
+              className="border-2 w-full rounded-lg p-2 outline-none border-gray-400"
+            />
+            {errors.splToken && <span>This field is required</span>}
+          </div>
+          <div className="py-2">
+            <label className="font-mono pl-4 text-sm">Memo</label>
+            <input
+              {...register("memo", { required: true })}
+              type="text"
+              className="border-2 w-full rounded-lg p-2 outline-none border-gray-400"
+            />
+            {errors.memo && <span>This field is required</span>}
+          </div>
 
-                    <div className='py-2'>
-                        <label className='font-mono pl-4 text-sm'>Message</label>
-                        <input {...register("message", { required: true })} type="text" className='border-2 w-full rounded-lg p-2 outline-none border-gray-400' />
-                        {errors.memo && <span>Message field is required</span>}
-                    </div>
-                    <div className='py-2'>
-                        <label className='font-mono pl-4 text-sm'>Token</label>
-                        <input {...register("splToken", { required: true })} type="text" className='border-2 w-full rounded-lg p-2 outline-none border-gray-400' />
-                        {errors.splToken && <span>This field is required</span>}
-                    </div>
-                    <div className='py-2'>
-                        <label className='font-mono pl-4 text-sm'>Memo</label>
-                        <input {...register("memo", { required: true })} type="text" className='border-2 w-full rounded-lg p-2 outline-none border-gray-400' />
-                        {errors.memo && <span>This field is required</span>}
-                    </div>
+          <div className="flex justify-center pt-2">
+            <input
+              type="submit"
+              className="p-2 bg-blue-400 px-10 rounded-md font-semibold font-serif"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-                    <div className='flex justify-center pt-2'>
-                        <input type="submit" className='p-2 bg-blue-400 px-10 rounded-md font-semibold font-serif' />
-                    </div>
-                </form>
-            </div>
-        </div>
-    )
-}
-
-export default QrCodeForm
+export default QrCodeForm;
--- app/components/RegisterForm.tsx
+++ app/components/RegisterForm.tsx
@@ -1,68 +1,83 @@
-"use client"
-import { useState } from 'react'
-import axios from 'axios'
-import { useRouter } from 'next/navigation'
-import React, { useEffect } from 'react'
-import { SubmitHandler, useForm } from 'react-hook-form'
-import { toast } from "react-hot-toast"
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import React, { useEffect } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
 
-const RegisterForm = () => { 
-    const router = useRouter()
-    const [errorrMessage, setErrorMessage] = useState(false)
+const RegisterForm = () => {
+  const router = useRouter();
+  const [errorrMessage, setErrorMessage] = useState(false);
 
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-    } = useForm<Merchant>()
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Merchant>();
 
-    
+  const onSubmit = async (data: Merchant) => {
+    const notification = toast.loading("Starting");
+    try {
+      const requestBody = {
+        recipient: { publicKey: data.merchantPublicKey },
+        devnet: true,
+      };
 
-    const onSubmit = async (data: Merchant) => {
-        const notification = toast.loading('Starting');
-        try {
-            const requestBody = {
-                recipient: { publicKey: data.merchantPublicKey },
-                devnet: true
-            };
+      const response = await axios.post(
+        "https://stable-pay-production.up.railway.app/tx/createSplit",
+        requestBody,
+      );
 
-            
-            const response = await axios.post("https://stable-pay-production.up.railway.app/tx/createSplit", requestBody);
-            
-            await localStorage.setItem('payAccount', response.data.split.payingAccount);
-            console.log(response.data.split.payingAccount)
-            toast.success('Account is successfully created', { id: notification });
-            router.push("/")
-        } catch (error) {
-            console.error('Error:', error);
-            toast.error('Error', { id: notification });
-            setErrorMessage(true)
-        }
-    };
+      await localStorage.setItem(
+        "payAccount",
+        response.data.split.payingAccount,
+      );
+      console.log(response.data.split.payingAccount);
+      toast.success("Account is successfully created", { id: notification });
+      router.push("/qr_code");
+    } catch (error) {
+      console.error("Error:", error);
+      toast.error("Error", { id: notification });
+      setErrorMessage(true);
+    }
+  };
 
-    useEffect(() => {
-        const payingAcct = localStorage.getItem('payAccount');
-        console.log("payingAcctpayingAcct",payingAcct)
-    }, [])
-
+  useEffect(() => {
+    const payingAcct = localStorage.getItem("payAccount");
+    console.log("payingAcctpayingAcct", payingAcct);
+  }, []);
 
   return (
-      <div className='h-full flex-1 py-2 flex items-center justify-center'>
-        <div className='h-full'>
-            <h2 className='text-center '>Enter Merchant public address</h2>
-              <form onSubmit={handleSubmit(onSubmit)} className="min-w-[400px] px-4">
-                  <input {...register("merchantPublicKey", { required: true })} type="text" className={` w-full border-2 rounded-lg p-2 outline-none ${errorrMessage ? " border-red-400" : " border-gray-400"}`}/>
-                  {errors.merchantPublicKey && <span className='text-red-500'>This field is required</span>}
-                  {errorrMessage && <p className='text-[11px] text-red-600'>Wallet address is not valid</p>}
+    <div className="h-full flex-1 py-2 flex items-center justify-center">
+      <div className="h-full">
+        <h2 className="text-center ">Enter Merchant public address</h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="min-w-[400px] px-4">
+          <input
+            {...register("merchantPublicKey", { required: true })}
+            type="text"
+            className={` w-full border-2 rounded-lg p-2 outline-none ${errorrMessage ? " border-red-400" : " border-gray-400"}`}
+          />
+          {errors.merchantPublicKey && (
+            <span className="text-red-500">This field is required</span>
+          )}
+          {errorrMessage && (
+            <p className="text-[11px] text-red-600">
+              Wallet address is not valid
+            </p>
+          )}
 
-                  <div className='flex justify-center pt-6'>
-                      <input type="submit" className={`p-2 bg-blue-400 px-10 rounded-md font-semibold font-serif`} />
-                  </div>
-              </form>
-        </div>
+          <div className="flex justify-center pt-6">
+            <input
+              type="submit"
+              className={`p-2 bg-blue-400 px-10 rounded-md font-semibold font-serif`}
+            />
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default RegisterForm
+export default RegisterForm;
